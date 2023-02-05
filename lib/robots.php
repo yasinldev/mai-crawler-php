e94dfe9a4453d30b8e48fe4edefcbb2f4477dfcf
M. Yasin Özkaya <90653146+yasinldev@users.noreply.github.com>
2023-02-05 23:26:43 +0300

    Add files via upload

--- lib/robots.php
+++ lib/robots.php
@@ -2,23 +2,25 @@
 
 namespace lib\readRobots;
 
-class robots{
+use helpers\robotstxtReader;
+
+@define("ROOT", dirname(__DIR__, 1)); // define root directory
+
+class robots extends robotstxtReader\readfile{
     public $host;
     public $scheme;
 
-    public array $disallow = [];
-    public array $allow = [];
 
     public function get_page_details(){
-        fopen("../json/parsedURL.json", "r") or die("Unable to open file!");
-        $file = file_get_contents("../json/parsedURL.json");
+        fopen(ROOT . "/json/parsedURL.json", "r") or die("Unable to open file!");
+        $file = file_get_contents(ROOT . "/json/parsedURL.json");
         $decode = json_decode($file, true);
 
         $this->host = $decode['host'];
         $this->scheme = $decode['scheme'];
     }
 
-    public function read_robots(){
+    public function curl_robots(){
         $host = $this->host;
         $sheme = $this->scheme;
         $url = "$sheme://".$host."/robots.txt";
@@ -29,40 +31,16 @@ class robots{
         $output = curl_exec($ch);
         $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        fopen("../json/robots/robots.txt", "w+") or die("Unable to open file!");
+        fopen(ROOT . "/json/robots/robots.txt", "w+") or die("Unable to open file!");
         if($status == 200){
-            file_put_contents("../json/robots/robots.txt", $output);
+            file_put_contents(ROOT . "/json/robots/robots.txt", $output);
         }else{
-            file_put_contents("../json/robots/robots.txt", "No robots.txt file found");
+            file_put_contents(ROOT . "/json/robots/robots.txt", "No robots.txt file found");
         }
     }
 
-    public function save_settings(){
-        $fl = fopen("../json/robots/robots.txt", "r") or die("Unable to open file!");
-        $file = file_get_contents("../json/robots/robots.txt");
-        $count = 0;
-        while(($line = fgets($fl)) !== false){
-            $count++;
-        }
-        for($i = 0; $i < $count; $i++) {
-            if(explode("#", $file)[$i] == "Comment"){
-                continue;
-            }
-            if (strpos($file, "Disallow: ") !== false) {
-                $disallow = explode("Disallow: ", $file);
-                @$this->disallow[] = $disallow[$i];
-            } elseif (strpos($file, "Allow: ") !== false) {
-                $allow = explode("Allow: ", $file);
-                @$this->allow[] = $allow[$i];
-            }
-        }
-        echo "<pre>";
-        print_r($this->disallow);
-        echo "</pre>";
-    }
 }
 
 $class = new robots();
 $class->get_page_details();
-$class->read_robots();
-$class->save_settings();
+$class->curl_robots();
